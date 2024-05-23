8ca86664429e5db79f09cc324954205eeb312dd2
Marcos Tavares <mjtdes.md@gmail.com>
2024-05-23 14:28:25 -0300

    feat: Fix the import errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="flex bg-slate-500 font-medium hover:bg-red-300">
+      <h1 className="flex flex-col bg-slate-500 font-medium text-white hover:bg-red-300">
         Testing
       </h1>
     </>
-  );
+  )
 }
